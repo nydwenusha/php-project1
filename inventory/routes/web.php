f97c55561512c5d9a2950ca7d14f3df1086cb8b8
Wenusha Rashindi <127926916+nydwenusha@users.noreply.github.com>
2026-08-09 15:46:40 +0530

    Update web.php

--- inventory/routes/web.php
+++ inventory/routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+
+// Route to view logs (temporary for debugging)
+Route::get('/logs', function () {
+    $logFile = storage_path('logs/laravel.log');
+    if (file_exists($logFile)) {
+        return '<pre>' . nl2br(file_get_contents($logFile)) . '</pre>';
+    }
+    return 'No log file found.';
+});
 
 Route::get('/{any?}', function () {
     if (file_exists(public_path('index.html'))) {
